20be57580664c9770668b7603e10029fd0adf306
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-09 06:08:57 +0000

    Calculate type sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print the sizes of variable types
+ *
+ * Description: Print the sizes of variable types using printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %u byte(s)\n", sizeof(long long));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
+	return (0);
+}
