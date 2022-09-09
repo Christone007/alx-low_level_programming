bcc8a1693445e048fb9dbbe0fa01382961e32565
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-09 05:26:29 +0000

    Add more documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Print the string in the put function
  *
+ * Description: using the main function
+ * This program prints a text using the printf function
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art, \n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
