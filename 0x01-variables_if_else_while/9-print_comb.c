bcff41cc87206daea81f3e3ade1cd1b1db16cb32
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-10 15:07:15 +0000

    Print 1 to 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+
+	return (0);
+}
