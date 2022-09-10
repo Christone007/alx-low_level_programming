31e71fd72e48b58005fd26945729fff819b58b48
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-10 05:04:54 +0000

    Add new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print letters of alphabet using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = '141';
+	while (c <= 172)
+		{
+
+
