6cc04ab6086a1668b01dc132ab5ee03056318d3b
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-10 14:47:56 +0000

    Print the hex digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print the Hexadecimal characters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hexa, hexa2;
+
+	hexa = '0';
+	hexa2 = 'a';
+
+	while (hexa < '9')
+	{
+		putchar(hexa);
+		hexa++;
+	}
+	while (hexa2 <= 'f')
+	{
+		putchar(hexa2);
+		hexa2++;
+	}
+	putchar('\n');
+
+	return (0);
+}
