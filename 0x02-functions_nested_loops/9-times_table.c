10de11f26c0c832708def11decbc9c052d6e2a58
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-14 19:32:16 +0000

    Print the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * times_table - Print the 9 times table
+ *
+ * Description: Print 0 -9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int n, m, prod, a, b;
+
+	n = 0;
+	while (n < 10)
+	{
+		m = 0;
+		while (m < 10)
+		{
+			prod = n * m;
+
+			a = prod / 10;
+			b = prod % 10;
+
+			if (m == 0)
+			{
+				_putchar(prod + '0');
+			}
+			else
+			{
+			if (a == 0)
+			{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(b + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(a + '0');
+				_putchar(b + '0');
+
+			}
+			}
+			if (m == 9)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+			_putchar(',');
+			}
+			m++;
+		}
+		n++;
+	}
+}
