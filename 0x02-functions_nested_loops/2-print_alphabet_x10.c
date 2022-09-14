bdad93eb665bd316153e50966771d95cc60a8738
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-14 03:06:57 +0000

    Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int n = 0;
+	while (n < 10)
+	{
+	print_alphabet();
+	n++;
+	}
+
+	return;
+}
+
+/**
+ * print_alphabet - Print a -z
+ *
+ * Description: Print the alphabet in lowercase
+ * Return: void
+ */
+void print_alphabet(void){
+	char n = 'a';
+	while (n <= 'z')
+	{
+		_putchar(n);
+	       	n++;
+	}
+	_putchar('\n');
+	return;
+}
