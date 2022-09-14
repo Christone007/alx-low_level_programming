b4ef9ebfee13bcab3e31ce19f670650ca2ab12f4
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-14 02:07:02 +0000

    Make betty compliant

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** main - Entry point
+/**
+ * main - Entry point
  *
  * Description: Print putchar
  * Return: Always (0);
@@ -14,7 +15,7 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r');	
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
