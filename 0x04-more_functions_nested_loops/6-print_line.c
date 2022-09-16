f6e8e3b7b69af166dec007979e2e3f63e4c4a08f
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-16 06:31:35 +0000

    Print a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - Print a line on the terminal
+ * @n: The number of underscores to use to 
+ * create the line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		i = 0;
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
+
