4e74fef8baaefd1ee9d28647ca2af606211b6388
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-22 02:26:02 +0000

    Print the second half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - Print the second half of a string
+ * @str: The string whose half is printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len, to_print;
+
+	len = 0;
+	while(str[len] != '\0')
+	{
+		len++;
+	}	
+	
+	if (len % 2 == 1)
+	{
+		to_print = (len / 2) + 1;
+	}
+	else
+	{
+		to_print = len / 2;
+	}
+
+	while (to_print < len)
+	{
+		_putchar(str[to_print]);
+		to_print++;
+	}
+	_putchar('\n');
+}
