31e69309770ba89f0de83d96ac89ce55a9b8ef02
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-22 21:00:36 +0000

    Change lowercase to Upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters of
+ * a string to uppercase
+ * @str: A char pointer to the string
+ *
+ * Return: a pointer to the string
+ */
+ char *string_toupper(char *str)
+{
+	unsigned int i;
+	int temp;
+
+	i = 0;
+	while ( str[i] != '\0')
+	{
+		temp = 0;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			temp = str[i] - '0';
+			temp -= 32;
+			str[i] = temp + '0';
+		}
+		i++;
+	}
+	return (str);
+
+}
