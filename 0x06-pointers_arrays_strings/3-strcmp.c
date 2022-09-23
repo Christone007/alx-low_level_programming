b4e8fe826e6328124b9e365906b553f2797a38a8
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-23 03:38:24 +0000

    Compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * getStringLength - get the length of a string
- * @s: The string whose length should be found
- *
- * Return: the length of the string as int
- */
-unsigned int getStringLength(char *s)
-{
-	unsigned int len;
-
-	len = 0;
-
-	if (s[0] == 0 + '0')
-		return (0);
-	while (s[len] != '\0')
-		len++;
-	return (len);
-}
-
-/**
  * _strcmp - Compares two stings
  * @s1: The string to be compared against another
  * @s2: The string to be compared against
@@ -28,16 +9,11 @@ unsigned int getStringLength(char *s)
  */
 int _strcmp(char *s1, char *s2)
 {
-	unsigned int l1, l2;
-
-	l1 = getStringLength(s1);
-	l2 = getStringLength(s2);
-
-	if (l1 > l2)
-		return (15);
-	else if (l1 == l2)
-		return (0);
-	else
-		return (-15);
+	while (*s1 && *s1 == *s2)
+	{
+		++s1;
+		++s2;
+	}
+	return (int)(unsigned char)(*s1) - (int)(unsigned char)(*s2);
 }
 
