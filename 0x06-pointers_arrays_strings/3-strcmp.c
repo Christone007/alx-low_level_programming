7b38c0352bcb00c73b47514acb3c484ee6a851dd
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-23 03:12:53 +0000

    Compare 2 strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * getStringLength - get the length of a string
+ * @s: The string whose length should be found
+ *
+ * Return: the length of the string as int
+ */
+int getStringLength(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * _strcmp - Compares two stings
+ * @s1: The string to be compared against another
+ * @s2: The string to be compared against
+ *
+ * Return: 1 if @s1 > @s2, 0 if @s1 == @s2, -1 if @s1 < @s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int l1, l2;
+
+	l1 = getStringLength(s1);
+	l2 = getStringLength(s2);
+
+	if(l1 > l2)
+		return (1);
+	else if(l1 == l2)
+		return (0);
+	else
+		return (-1);
+}
+
