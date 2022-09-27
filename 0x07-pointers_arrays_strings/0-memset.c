c47bbb53e5e5d5b1ecc2e9a6ee7ed7f94bd9fac1
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-27 02:13:34 +0000

    Create memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - Fills memory with a constant byte.
+ * @s: A pointer to the memory area
+ * @b: The constant byte
+ * @n: The size of the memory area to fill
+ *
+ * Return: a pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*s = b;
+		i++;
+		s++;
+	}
+	return (s);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,12 +1,12 @@
 ## Welcome to Directory 0x07
 
-### Tasks
-
-### Resources  
+### Tasks  
 0. Memset  
  A function that fills memory with a constant byte
  > char *_memset(char *s, char b, unsigned int n);
 1. Memcpy 
+
+### Resources
 
 ### My thoughts
 * What would one need an array of more than 3 dimensions for?
