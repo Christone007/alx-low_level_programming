d2e6dd488ff8192dfa7a725d424b64dc596ca7e0
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-09-29 13:14:59 +0000

    Print a string recursively

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,39 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * getLength - calculate the length of a string
- * @s: pointer to the string
- *
- * Return: length of the string
- */
-unsigned int getLength(char *s)
-{
-	int i;
-
-	i = 0;
-	if(*(s + i) != '\0')
-	{
-		return (1 + getLength(s++));	
-	}
-	return (0);
-}
-
-/**
- * _print_rev_recursion - Print a string in reverse
- * @s: The pointer to the string
- *
- * Return: void
- */
+* _print_rev_recursion - Print a string in reverse
+* @s: The string to print
+*
+* Return: void
+*/
 void _print_rev_recursion(char *s)
 {
-	if(*s == '\0')
+	char first;
+
+	if (*s != '\0')
 	{
-		_putchar(*(s - 1));
-		_print_rev_recursion(s - 1);
-	}
-	if(*s != '\0')
-	{
-		_print_rev_recursion(s + 1);
+		first = *s;
+		s++;
+
+		_print_rev_recursion(s);
+		_putchar(first);
 	}
 }
+
