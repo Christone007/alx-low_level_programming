159e14c5aaef4da6f73cc1940938ea0ea5665cdf
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-07 21:28:07 +0000

    Allocate memory and initialize it

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocate memory for an array
+ * @nmemb: Number of elements
+ * @size: Size of each element
+ *
+ * Description: Initialize the memory bytes to 0.
+ *
+ * Return: a Void pointer to the allocated memory, if error return NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, nb;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return NULL;
+
+	nb = nmemb * size;
+
+	ptr = malloc(nb);
+
+	if (ptr == NULL)
+		return NULL;
+
+	i = 0;
+	while (nb--)
+	{
+		*(ptr + i) = '\0';
+		i++;
+	}
+
+	return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /*MAIN_H*/
