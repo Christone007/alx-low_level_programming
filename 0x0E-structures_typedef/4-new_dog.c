ff15972c161c8bcf8edca423d0730c7d3ef6c57e
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-12 07:31:52 +0000

    CoMake buffer coopies of name and owner arguments

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,43 @@
 #include "dog.h"
 
+/**
+ * _strlen - Calculates the length of a string 
+ * @str: The string
+ *
+ * Return: The length of the string excluding the terminal null byte
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
 
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+
+/**
+ * _strcpy - Make a copy of a string
+ * @str: The string to copy
+ * @buf: The memory location copy it to
+ *
+ * Return: pointer to the @buf
+ */
+char *_strcpy(char *buf, char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		buf[i] = str[i];
+		i++;
+	}
+	buf[i] = '\0';
+	return (buf);
+}
 
 
 /**
@@ -14,19 +51,24 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	char *name_copy;
+	char *owner_copy;
+
 
 	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
-	{
-		free(name);
-		free(owner);
 		return (NULL);
-	}
 
-	dog->name = name;
+	name_copy = malloc(sizeof(char) * _strlen(name));
+	owner_copy = malloc(sizeof(char) * _strlen(owner));
+
+	name_copy = _strcpy(name_copy, name);
+	owner_copy = _strcpy(owner_copy, owner);
+
+	dog->name = name_copy;
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = owner_copy;
 
 	return (dog);
 }
