886128dc8c1396cbb94ee9ba7bdbad09e16237bb
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-13 04:45:49 +0000

    Pass the elements of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Calls a given function for each element of an array
+ * @array: The array
+ * @size: The number of elements in @array
+ * @action: The pointe to the function to use
+ *
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (size < 1 || array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
