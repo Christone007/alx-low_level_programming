03b0462b1a5f6c58a21ae8bbc8a36c47487394c6
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-13 14:01:56 +0000

    Print OPCODES

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Print the opcodes of the main function
+ * @argc: Argument count
+ * @argv: The Argument vector
+ *
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+	int nbytes, i;
+	unsigned char *ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	ptr = (unsigned char *)main;
+	i = 0;
+
+	while (i < nbytes)
+	{
+		printf("%02x ", ptr[i]);
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
