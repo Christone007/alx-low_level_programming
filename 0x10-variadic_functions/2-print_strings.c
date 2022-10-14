5bb563aee7874250de641683edec1654c7000e75
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-14 03:25:51 +0000

    Write function that prints strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings
+ * @separator: The string seperator
+ * @n: The number of string variables
+ *
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int count;
+	char *str;
+
+	if (n < 1)
+		return;
+
+	va_start(ap, n);
+
+	for (count = 0; count < n; count++)
+	{
+		str = va_arg(ap, char *);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s", str);
+
+		if (count + 1 != n && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -10,9 +10,11 @@ the use of variadic functions
 ## Tasks
 0. Write a function that returns the sum of all its parameters  
 1. Write a function that prints numbers, followed by a new line.
+2. Write a function that prints strings, followed by a new line.
 
 ## Favourite Task Quotes
 0. Beauty is variable, ugliness is constant
 1. To be is to be the value of a variable
+2. One woman's constant is another woman's variable
 
 ## Extras
