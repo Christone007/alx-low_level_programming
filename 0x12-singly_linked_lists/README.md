1b7e0a68b336c581ba7eb9e7f2cc1bc65f5f1566
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-20 19:55:20 +0000

    Initialize Task

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,21 @@
+# SINGLY LINKED LISTS
+
+## Introduction
+Singly linked lists are a type of derived data structures
+which is made up of nodes. Each node contains a field for
+data as well as a pointer to the next node. The first node
+is the HEAD and the last node points to NULL.
+
+singly linked lists are the second most-used data structures
+after arrays.
+
+It is easy to add\delete nodes from lists compared to arrays
+however, it is not very efficient to search compared to arrays
+
+## Tasks
+The following tasks were done as part of a Holberton School
+software Engineering Program
+
+## Quotes
+
+## Extras
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,33 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The char to write
+ * Return: The number of bytes written
+ */
+int _putchar(char c);
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: The list to print
+ * Return: The number of nodes in the list
+ */
+size_t print_list(const list_t *h);
+
+#endif /*LISTS_H*/
