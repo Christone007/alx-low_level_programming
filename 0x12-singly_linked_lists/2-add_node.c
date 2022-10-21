1091ee567221bc81a7df5b0c8dac55ab2f837241
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-21 03:36:00 +0000

    Create a function that adds new nodes at the beginning of a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * _strlen - Gets the length of a string
+ * @str: The string whose length is to be found
+ *
+ * Return: The length of the string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	
+	return (i);
+}
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: The head of the list
+ * @str: The string to be saved in the node
+ *
+ * Return: Pointer to the new element, NULL if fail
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *new;
+
+	new = (list_t) malloc(sizeof(list_t));
+
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = &head;
+
+	head = new;
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -41,4 +41,13 @@ size_t print_list(const list_t *h);
  */
 size_t list_len(const list_t *h);
 
+/**
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: The head of the list
+ * @str: The string to be saved in the node
+ *
+ * Return: Pointer to the updated list
+ */
+list_t *add_node(list_t **head, const char *str);
+
 #endif /*LISTS_H*/
