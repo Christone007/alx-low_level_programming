ad959c3c0ad9b97dfedb371e32e48211d1d48e61
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-21 10:16:32 +0000

    Write an assembly code file

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+; This is an assembly program
+
+global _start
+
+section .rodata
+message: db "Hello, Holberton", 0xa
+message_len equ $-message
+
+section .text
+_start:
+mov rax, 1
+mov rdi, 1
+mov rsi, message
+mov rdx, message_len
+syscall
+
+mov rax, 60
+mov rdi, 0
+syscall
