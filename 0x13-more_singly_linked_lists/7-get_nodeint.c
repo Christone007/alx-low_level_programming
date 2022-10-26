0deb2d5a11e3872d68e84faf97b5f3dbdd900e36
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-26 00:23:35 +0000

    Create function to find a node using index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Finds and returns a node at particular index
+ * @head: The head of the linked list
+ * @index: The index of the node to find
+ *
+ * Return: The node if found, NULL if not found
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	i = 0;
+	while (head->next != NULL)
+	{
+		if (i == index)
+			return (head);
+
+		i++;
+		head = head->next;
+	}
+
+	return (NULL);
+}
