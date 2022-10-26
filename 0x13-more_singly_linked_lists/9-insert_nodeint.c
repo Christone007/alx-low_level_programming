6116a0b4f2675ab7e02b84a43b6a0f643f7faaac
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-26 03:27:06 +0000

    Refactor code with a function to insert any node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,29 @@
 #include "lists.h"
-#include <stdio.h>
+
+/**
+ * insert_any_node - Inserts a node
+ * @anchor: The preceeding node which would point to new node
+ * @node: The node to insert
+ *
+ * Return: Address of inserted node
+ */
+listint_t *insert_any_node(listint_t *anchor, listint_t *node)
+{
+	if (anchor == NULL)
+	{
+		anchor = node;
+	}
+	else if (anchor->next == NULL)
+	{
+		anchor->next = node;
+	}
+	else
+	{
+		node->next = anchor->next;
+		anchor->next = node;
+	}
+	return (node);
+}
 
 /**
  * insert_nodeint_at_index - Inserts a node at a given index
@@ -15,7 +39,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	ptr = *head;
-
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -25,12 +48,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (ptr == NULL)
 	{
-		/*Insert new node at beginning*/
 		if (idx == 0)
-		{
-			*head = new_node;
-			return (*head);
-		}
+			return (insert_any_node(*head, new_node));
 		else
 			return (NULL);
 	}
@@ -46,24 +65,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (ptr->next != NULL)
 	{
 		if (i == idx - 1)
-		{
-			/*Insert new node in front*/
-			new_node->next = ptr->next;
-			ptr->next = new_node;
-			ptr = new_node;
-			return (ptr);
-		}
+			return (insert_any_node(ptr, new_node));
 		i++;
 		ptr = ptr->next;
 	}
 
 	if (i == idx - 1)
-	{
-		/*insert at end*/
-		ptr->next = new_node;
-		ptr = new_node;
-		return (ptr);
-	}
+		return (insert_any_node(ptr, new_node));
 
 	return (NULL);
 }
