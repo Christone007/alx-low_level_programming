020eff8c1381a0ab63e9790c79bc86e92c8fc5a3
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-28 05:06:07 +0000

    Fix zero printing error

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,16 +8,25 @@
  */
 void print_binary(unsigned long int n)
 {
-	int bin_digit, bit_size;
+	int bin_digit, bit_size, print;
 
+	print = 0;
 	bit_size = sizeof(unsigned long int) * 8;
 
 	while (bit_size--)
 	{
 		bin_digit = n >> bit_size;
 		if (bin_digit & 1)
+		{
+			print = 1;
 			_putchar(49);
+		}
 		else
-			_putchar(48);
+		{
+			if(print)
+				_putchar(48);
+		}
 	}
+	if (!print)
+		_putchar(48);
 }
