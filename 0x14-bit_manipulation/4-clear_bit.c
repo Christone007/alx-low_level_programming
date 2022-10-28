2dea86ba91e10ff3f39cb9861603750cd721698b
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-28 10:15:02 +0000

    Clear bit only if it was ON

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n = *n ^ (1 << index);
+	/*Checks if the byte at that index is ON, then switches it OFF*/
+	if (*n & 1 << index)
+		*n = *n ^ (1 << index);
 
 	return (1);
 }
