203c03b92657372bfac787f074b070bce9300a35
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-11-01 23:42:05 +0000

    Check close fd response

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t text_length, bytes_written;
-	int fd;
+	int fd, cl;
 
 	if (filename == NULL)
 		return (-1);
@@ -47,6 +47,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	close(fd);
+	cl = close(fd);
+	if (cl == -1)
+		return (-1);
+
 	return (1);
 }
