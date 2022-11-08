6d32e020df01dcbaf1fbc3e86be795c1051b0709
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-11-08 00:55:17 +0000

    Shrink number of lines by creating printer function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,23 +1,50 @@
 #include "main.h"
 
 /**
- * close_fd - Closes a file descriptor
- * @fd: The file descriptor to close
+ * print_stderr_exit - Print a message to standard error and exit program
+ * @msg: The message to print
+ * @code: The exit code
+ * @var: Any variable to be printed in a format specifier
  *
  * Return: void
  */
-void close_fd(int fd)
+void print_stderr_exit(char *msg, int code, void *var)
+{
+	dprintf(STDERR_FILENO, msg, var);
+	exit(code);
+}
+
+/**
+ * close_fd - Closes a file descriptor
+ * @fd1: The file descriptor to close
+ * @fd2: The second desctiptor to close
+ *
+ * Return: void
+ */
+void close_fd(int fd1, int fd2)
 {
 	int cl;
 
-	cl = close(fd);
-
-	if (cl == -1)
+	if (fd1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
-		exit(100);
+		cl = close(fd1);
+		if (cl == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
+			exit(100);
+		}
 	}
 
+	if (fd2)
+	{
+		cl = close(fd2);
+		if (cl == -1)
+		{
+
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd2);
+			exit(100);
+		}
+	}
 }
 
 /**
@@ -30,63 +57,44 @@ void close_fd(int fd)
  */
 int main(int argc, char **argv)
 {
-	char *file_from, *file_to, *buffer;
+	char *file_from = argv[1], *file_to = argv[2], *buffer;
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
-
-	file_from = argv[1];
-	file_to = argv[2];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);		
-		exit(98);
+		print_stderr_exit("Error: Can't read from file %s\n", 98, file_from);
 	}
-
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 00664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		close_fd(fd_from);
-		exit(99);
+		close_fd(fd_from, 0);
+		print_stderr_exit("Error: Can't write to %s\n", 99, file_to);
 	}
-
 	buffer = malloc(1024);
-
-	do
-	{
+	do {
 		bytes_read = read(fd_from, buffer, 1024);
 		if (bytes_read == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-			close_fd(fd_from);
-			close_fd(fd_to);
+			close_fd(fd_from, fd_to);
 			exit(98);
 		}
-
-		if (bytes_read > 0)
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
 		{
-			bytes_written = write(fd_to, buffer, bytes_read);
-			if (bytes_written == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-				close_fd(fd_from);
-				close_fd(fd_to);
-				exit(99);
-			}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			close_fd(fd_from, fd_to);
+			exit(99);
 		}
 	} while (bytes_read > 0);
-
-	close_fd(fd_from);
-	close_fd(fd_to);
+	close_fd(fd_from, fd_to);
 	free(buffer);
 	return (0);
 }
