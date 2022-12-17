c0edaa1c2a99b0f262dec4acb229d9cd99243966
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-17 10:31:03 +0000

    Refactor code to use functions from other files

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,43 +10,23 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *ptr, *newnode, *tmp;
+	dlistint_t *ptr = *h, *newnode, *tmp;
 	size_t count = 0;
 
-	ptr = *h;
-
-	newnode = malloc(sizeof(dlistint_t));
-	if (newnode == NULL)
-		return (NULL);
-
-	newnode->next = NULL;
-	newnode->prev = NULL;
-	newnode->n = n;
-
-	if (*h == NULL)
-	{
-		if (idx == 0)
-		{
-			*h = newnode;
-			return (newnode);
-		}
-		else
-			return (NULL);
-	}
-	else if (idx == 0)
-	{
-		newnode->next = *h;
-		(*h)->prev = newnode;
-		(*h) = newnode;
-		return (newnode);
-	}
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
 	while (ptr != NULL && count <= idx)
 	{
 		if (count == idx)
 		{
+			newnode = malloc(sizeof(dlistint_t));
+			if (newnode == NULL)
+				return (NULL);
+			newnode->n = n;
 			newnode->next = ptr;
 			newnode->prev = ptr->prev;
+
 			tmp = ptr->prev;
 			ptr->prev = newnode;
 			tmp->next = newnode;
@@ -60,10 +40,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (count + 1 == idx)
 	{
-		newnode->prev = ptr;
-		ptr->next = newnode;
-		return (newnode);
+		if ((*h) == NULL)
+			return (NULL);
+		else
+			return (add_dnodeint_end(h, n));
 	}
-
 	return (NULL);
 }
