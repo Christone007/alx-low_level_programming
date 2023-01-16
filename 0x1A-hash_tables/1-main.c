8a369d9e76030f30ab3d9ac41bbe4b067ce00357
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-01-16 18:51:28 +0000

    altered main

--- 0x1A-hash_tables/1-main.c
+++ 0x1A-hash_tables/1-main.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always EXIT_SUCCESS.
  */
@@ -12,11 +11,7 @@ int main(void)
 {
 	char *s;
 
-	s = "cisfun";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	s = "Don't forget to tweet today";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	s = "98";
+	s = "Holberton School !";
 	printf("%lu\n", hash_djb2((unsigned char *)s));
 	return (EXIT_SUCCESS);
 }
