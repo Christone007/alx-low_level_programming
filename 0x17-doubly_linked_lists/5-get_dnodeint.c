26c3b460eefcbc0f4746101351781a3880379167
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-12-16 06:58:32 +0000

    get the nth node of a doubly linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Gets the node at a certain index
+ * @head: The head of the doubly linked list
+ * @index: The index of the node to get
+ *
+ * Return: The node at the specified index. NULL if not exists
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr;
+	unsigned int count;
+
+	count = 0;
+	ptr = head;
+
+	while (ptr != NULL && count <= index)
+	{
+		if (count == index)
+			return (ptr);
+		count++;
+		ptr = ptr->next;
+	}
+
+	return (NULL);
+}
