944e60af343cafcd4addc7448fa16496fd97082a
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-12-16 08:11:46 +0000

    Revert to previous code

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo -e "#include <stdio.h>\nvoid srand(unsigned int seed) {}\nint rand() {return 1;}" > fake.c;
-gcc -shared -o libfake.so -fPIC fake.c; export LD_PRELOAD=$PWD/libfake.so; sleep 97
+cp /bin/true ./gm
+sleep 97
